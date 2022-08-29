b9d78570735c95d0ba145448619b173ff5c9901b
aleeeexis <alexis.vazquez.ml@gmail.com>
2022-08-29 12:47:23 -0300

    se agrega proyecto

---
+++ y/js/swiper.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    speed: 2000,
+    //effect: 'fade',
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+});
